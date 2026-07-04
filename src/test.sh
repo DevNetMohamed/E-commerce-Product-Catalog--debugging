7c4728a75e36422aa8da171c0748db7ffe9fc293
Mohamed Adel <mohamedadelhareidy@gmail.com>
2026-07-04 15:58:08 +0300

    CI Test Pipeline NodeJS App and Test V@

--- src/test.sh
+++ src/test.sh
@@ -2,18 +2,12 @@
 
 EXPECTED="Hello, Test"
 
-OUTPUT=$(node -e "console.log(require('src/app.js')('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app.js')('Test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
-
-echo "🫱 Test Passed!"
-
-exit 0
-
+  echo "🫱 Test Passed!"
+  exit 0
 else
-
-echo "⛔ Test Failed EXPECTED '$EXPECTED' but got '$OUTPUT'"
-
-exit 1
-
+  echo "⛔ Test Failed EXPECTED '$EXPECTED' but got '$OUTPUT'"
+  exit 1
 fi
