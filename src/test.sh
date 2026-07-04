e6bf3970caa10b91410fb6f9f423584ef5840eb2
Mohamed Adel <mohamedadelhareidy@gmail.com>
2026-07-04 14:33:07 +0300

    Adds the test Workflow3

---
+++ src/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+EXPECTED="Hello, Test"
+
+OUTPUT=$(node -e "console.log(require('../src/app.js')('Test'))")
+
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+
+echo "🫱 Test Passed!"
+
+exit 0
+
+else
+
+echo "⛔ Test Failed EXPECTED '$EXPECTED' but got '$OUTPUT'"
+
+exit 1
+
+fi
